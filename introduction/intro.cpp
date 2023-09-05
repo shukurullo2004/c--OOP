843568afdbfc4da2603f543fd6757d20a491d59d
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-09-05 14:49:09 +0900

    first commit

---
+++ introduction/intro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a, b;
+    // a =5;
+    // b = 4;
+    // cout << a+ (++b)<< endl;
+
+
+
+    // int a  = 20; 
+    // int b = 10;
+    // a = a+b;
+    // b = a-b;
+    // a = a-b;
+    // cout << a << " " << b << endl;
+    return 0;
+}
