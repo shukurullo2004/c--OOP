74b1d0b75d4cef15dd2e5798e1876e370293fba3
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-09-05 17:32:46 +0900

    improvments for counting digits

--- introduction/counting digit.cpp
+++ introduction/counting digit.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int number;
+    long int number;
     int counter = 0;
     cout << "Enter a number: ";
     cin >> number;
@@ -14,12 +14,18 @@ int main(){
         }
         cout << "The number of digits is: " << counter << endl;
         }
-
     else if(number == 0){
         cout << "The number of digits is: 1" << endl;
     }
-    else 
-        cout <<"Please enter valid number!" << endl;
+
+    else{
+        number = number * -1;
+        while(number>0){
+        number = number / 10;
+        counter++;
+    }
+    cout << "The number of digits is: " << counter << endl;
+    }
 
     return 0;
 }
