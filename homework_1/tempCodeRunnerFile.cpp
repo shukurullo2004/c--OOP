ec4c0ab312f80d66d17cc42b67c244ed671ab962
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-09-18 10:21:21 +0900

    template

---
+++ homework_1/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ template <typename T, typename U>
+ auto max(T num1, U num2){
+    return (num1>num2)?num1:num2;
+ }
+int main(){
+   cout << max(2.3,1) << endl;
+
+    return 0;
+}
