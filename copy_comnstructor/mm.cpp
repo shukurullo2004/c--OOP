5c6e02c4155d83c4c761b091bc7dfdece9a8e204
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-10-15 18:48:33 +0900

    .

--- copy_comnstructor/mm.cpp
+++ copy_comnstructor/mm.cpp
@@ -1,1 +1,16 @@
-#include
+#include <iostream>
+using namespace std;
+
+class Student{
+    private:
+        string ism;
+        int id;
+        string *qiziqish;
+    public:
+        Student(string name, int iid, string *interest){
+            ism = name;
+            id = iid;
+            *qiziqish = new string[];
+        }
+
+};
