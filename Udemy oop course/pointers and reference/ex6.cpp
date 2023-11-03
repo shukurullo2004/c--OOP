dd62f44773412616c24a308510e00601eb8ed69b
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-04 00:18:57 +0900

    Shukurullo

---
+++ Udemy oop course/pointers and reference/ex6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int *apply_all(int* array1, int size1, int* array2, int size2, int& size) {
+    size = size1 * size2;
+    int *result = new int[size]; 
+
+    for (int i = 0; i < size1; i++) {
+        for (int j = 0; j < size2; j++) {
+            result[i * size2 + j] = array1[i] * array2[j];
+        }
+    }
+    return result;
+}
+
+void print(int *result, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int array1[] = {1, 2, 3, 4, 5};
+    int size1 = 5;
+    int size2 = 3;
+    int size = 0; // Initialize size to 0.
+    int array2[] = {10, 20, 30};
+    int *result = nullptr; // Initialize result to nullptr.
+
+    result = apply_all(array1, size1, array2, size2, size);
+    print(result, size);
+
+    delete[] result; // Deallocate the dynamically allocated memory.
+
+    return 0;
+}
