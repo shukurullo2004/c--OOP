10e6250bf217497bf8cc57feb1d34fa60a891333
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-06 08:33:52 +0900

    home

--- homework_6/ex5/main.cpp
+++ homework_6/ex5/main.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-#include<iomanip>
-#include<ctime>
-#include<cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
 class IntArray
 {
 private:
@@ -15,23 +16,49 @@ public:
     {
         m_data = new int[m_len];
     }
+
     ~IntArray() {
         if (m_data) delete[] m_data;
     }
+
+    // Friend functions to set and display the array
+    friend void setArray(IntArray& arr, int* data, int len);
+    friend void displayArray(const IntArray& arr);
 };
 
+void setArray(IntArray& arr, int* data, int len)
+{
+    if (arr.m_data && len == arr.m_len) {
+        for (int i = 0; i < len; ++i) {
+            arr.m_data[i] = data[i];
+        }
+    }
+    else {
+        cout << "Invalid array or length provided for setting data." << endl;
+    }
+}
+
+void displayArray(const IntArray& arr)
+{
+    if (arr.m_data) {
+        for (int i = 0; i < arr.m_len; ++i) {
+            cout << arr.m_data[i] << " ";
+        }
+        cout << endl;
+    }
+    else {
+        cout << "Array is empty." << endl;
+    }
+}
 
 const int arSize = 20;
-
-
-
 
 int main() {
     int i;
     int data[arSize];
     IntArray ar{ arSize };
 
-    srand((unsigned int)time(NULL));
+   srand(static_cast<unsigned int>(time(nullptr)));
     for (i = 0; i < arSize; ++i) data[i] = rand() % 100;
 
     setArray(ar, data, arSize);
