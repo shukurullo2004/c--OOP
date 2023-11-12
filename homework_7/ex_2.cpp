c6f268a5634b1e6587d5f278d89bd22e1cd0a8fa
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-12 19:43:52 +0900

    ex_2

---
+++ homework_7/ex_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    public:
+    int data;
+    int ID;
+    static int numberOfObjects;
+    Test(int data):data{data}, ID{numberOfObjects++}{ };
+
+    int getData(){
+        return data;
+    }
+    int getID(){
+        return ID;
+    }
+    int static NumOfObjects(){
+        return numberOfObjects;
+    }
+};
+int Test::numberOfObjects = 0;
+
+int main() {
+    cout << "# of objects: ";
+    cout << Test::NumOfObjects() << endl;
+
+    Test t1{ 10 }, t2{ 20 }, t3{ 30 }, t4{ 40 };
+    cout << "[t1] data: " << t1.getData() << ", id: " << t1.getID() << endl;
+    cout << "[t2] data: " << t2.getData() << ", id: " << t2.getID() << endl;
+    cout << "[t3] data: " << t3.getData() << ", id: " << t3.getID() << endl;
+    cout << "[t4] data: " << t4.getData() << ", id: " << t4.getID() << endl;
+
+    cout << "# of objects: ";
+    cout << Test::NumOfObjects() << endl;
+    return 0;
+}
