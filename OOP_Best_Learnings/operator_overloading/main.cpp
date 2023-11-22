35a90ba1c2f4414745d0c1a81dfc240b1db125c5
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-22 13:04:17 +0900

    learning

--- OOP_Best_Learnings/operator_overloading/main.cpp
+++ OOP_Best_Learnings/operator_overloading/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Counter{
+    friend Counter operator+(Counter c1, Counter c2);
     private:
 int count;
     public:
@@ -10,15 +11,30 @@ int count;
         count++;
     }
     void print(){
-        cout << count << endl;
+        cout <<"Num: "<<count << endl;
     }
+    // inside of class operator+;
+
+    // Counter operator+(Counter const& counter){
+    //     Counter sum = (count+counter.count);
+    //     return sum;
+    // }s
+ 
 };
+
+// operator + ouyside of class;
+Counter operator+(Counter c1, Counter c2){
+    Counter sum = c1.count+c2.count;
+    return sum;
+ }
 int main(){
-    Counter number1(4);
+    Counter number1(3);
     number1.increment();
     number1.print();
-
-
+    Counter number2(6);
+    number2.print();
+    Counter number3 = number1 + number2;
+    number3.print();
     return 0;
 }
 
