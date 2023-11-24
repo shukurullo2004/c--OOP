228e3bbc7e970649d2c172b50379a95bd5bc95fb
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-24 23:13:13 +0900

    ex_3

--- homework_9/ex_2/main.cpp
+++ homework_9/ex_2/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 class Friction{
-   friend ostream&operator<<(ostream&COUT , const Friction &f);
-   friend Friction operator*(Friction &f1 ,Friction &f2);
-   friend Friction operator*(Friction &f2, int m);
-friend Friction operator*(int m, Friction &f2);
+    friend ostream&operator<<(ostream&COUT , const Friction &f);
+    friend Friction operator*(Friction &f1 ,Friction &f2);
+    friend Friction operator*(Friction &f2, int m);
+    friend Friction operator*(int m, Friction &f2);
+    // friend Friction operator*(int m, Friction &f1,Friction &f2, int n);
 private:
 int m_numerator;
 int m_denominator;
@@ -30,6 +31,11 @@ Friction operator*(int m, Friction &f2){
     int denominator = (f2.m_denominator);
     return Friction(numerator,denominator);
 }
+// Friction operator*(int m, Friction &f1,Friction &f2, int n){
+//     int numerator = (m * f2.m_numerator * f1.m_numerator * n);
+//     int denominator = (f2.m_denominator * f1.m_denominator);
+//     return Friction(numerator,denominator);
+// }
 
 int main(){
     Friction f1{1,2}, f2{3,4};
@@ -37,7 +43,7 @@ int main(){
     cout << f1 * f2 << endl;
     cout << f1 * 2 << endl;
     cout << 3 * f2 << endl;
-    cout << 2*f1*f2*3<< endl;
+    // cout << 2 * (f1 * f2) * 3 << endl;
 
 
 return 0;
--- homework_9/ex_2/tempCodeRunnerFile.cpp
+++ homework_9/ex_2/tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
-numerator,denominator
+
+// }
