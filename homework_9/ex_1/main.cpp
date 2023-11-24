bb2d6d0002d75ec880ef1c895c2211d149cf445e
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-11-24 11:07:52 +0900

    Ex_1 is completed

---
+++ homework_9/ex_1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Friction{
+   friend ostream&operator<<(ostream&COUT , const Friction &f);
+private:
+int m_numerator;
+int m_denominator;
+public :
+Friction(int numerator= 0, int denominator = 1):m_numerator{numerator},m_denominator{denominator}{};
+
+
+};
+ostream&operator<<(ostream&COUT ,const Friction &f){
+    COUT <<f.m_numerator <<"/" << f.m_denominator;
+    return COUT;
+}
+int main(){
+    Friction f1{1,2}, f2{3,4};
+    cout << f1 << ", " << f2 << endl;
+return 0;
+}
