0c64e74261429b5e244494408b438c6bf8cda04c
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-12-09 14:34:57 +0900

    lab6

--- final_exam/lab6/main.cpp
+++ final_exam/lab6/main.cpp
@@ -1,53 +1,64 @@
-
 #include <iostream>
-using namespace std;
 #include <string>
 
-class SecuString{
-    public:
-    string name;
-    string password;
-    SecuString(string names, string passwords):name{names}, password{passwords}{};
+class SecuString {
+private:
+    std::string name;
+    std::string password;
 
-    void GetMessage(string ppassword){
-        if(password == ppassword){
-            cout <<"Password is correct. Name is "<< name << endl;
-        }
-        else{
-            cout << "Invalid password "<< endl;
+public:
+    // Constructor to initialize name and password
+    SecuString(const std::string& names, const std::string& passwords) : name{names}, password{passwords} {};
+
+    // Getter for name
+    const std::string& GetName() const {
+        return name;
+    }
+
+    // Method to check if the provided password matches the stored password
+    void GetMessage(const std::string& ppassword) const {
+        if (password == ppassword) {
+            std::cout << "Password is correct. Name is " << name << std::endl;
+        } else {
+            std::cout << "Invalid password" << std::endl;
         }
     }
 
-    void SetMessage(string nname, string npassword ){
+    // Setter for name and password
+    void SetMessage(const std::string& nname, const std::string& npassword) {
         name = nname;
         password = npassword;
-        }
+        std::cout << "Name and password set successfully." << std::endl;
+    }
 
-    void ChangePW(string old_password, string new_password){
-        if(old_password == password){
+    // Method to change the password if the old password matches
+    void ChangePW(const std::string& old_password, const std::string& new_password) {
+        if (old_password == password) {
             password = new_password;
-        }
-        else{
-            cout << "wrong password" << endl;
+            std::cout << "Password changed successfully." << std::endl;
+        } else {
+            std::cout << "Wrong password" << std::endl;
         }
     }
 };
+
 int main() {
+    // Creating an instance of the SecuString class
     SecuString msg("InhaUniv.", "password1");
 
     // Testing GetMessage
-    std::cout << "1. " << msg.GetMessage("wrongpassword") << std::endl;
-    std::cout << "2. " << msg.GetMessage("password1") << std::endl;
+    std::cout << "1. "; msg.GetMessage("wrongpassword");  // Output: Invalid password
+    std::cout << "2. "; msg.GetMessage("password1");      // Output: Password is correct. Name is InhaUniv.
 
     // Testing SetMessage
-    std::cout << "3. " << msg.SetMessage("INHAUNIV.", "wrongpassword") << std::endl;
-    std::cout << "4. " << msg.GetMessage("password1") << std::endl;
-    std::cout << "5. " << msg.SetMessage("INHAUNIV.", "password1") << std::endl;
-    std::cout << "6. " << msg.GetMessage("password1") << std::endl;
+    std::cout << "3. "; msg.SetMessage("INHAUNIV.", "wrongpassword");  // Setting new name and password
+    std::cout << "4. "; msg.GetMessage("password1");                   // Output: Invalid password
+    std::cout << "5. "; msg.SetMessage("INHAUNIV.", "password1");      // Setting new name and password
+    std::cout << "6. "; msg.GetMessage("password1");                   // Output: Password is correct. Name is INHAUNIV.
 
     // Testing ChangePW
-    std::cout << "7. " << msg.ChangePW("password1", "newpassword") << std::endl;
-    std::cout << "8. " << msg.GetMessage("newpassword") << std::endl;
+    std::cout << "7. "; msg.ChangePW("password1", "newpassword");     // Changing password
+    std::cout << "8. "; msg.GetMessage("newpassword");                // Output: Password is correct. Name is INHAUNIV.
 
     return 0;
 }
